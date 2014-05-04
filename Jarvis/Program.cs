bf373b391971604684dc76523c8bea9ca61cb458
Michael Salvia <michael.salvia@gmail.com>
2014-05-04 02:10:12 -0700

    * started interface design * started voice recognition engine

--- Jarvis/Program.cs
+++ Jarvis/Program.cs
@@ -16,7 +16,7 @@ namespace Jarvis
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Terminal());
         }
     }
 }
