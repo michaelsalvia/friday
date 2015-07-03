af82da85eb7ac470b927acdc666b73491ccf6785
Mike Salvia <salvia@epictek.net>
2015-07-02 20:39:26 -0700

    * change name to Friday

--- Jarvis/Program.cs
+++ Jarvis/Program.cs
@@ -5,89 +5,23 @@ using System.Speech.Recognition;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Jarvis
+namespace Friday
 {
 
     static class Program
     {
 
-        static Terminal jarvis = null;
-[STAThread]
+        static FridayConsole fridayConsole = null;
+        [STAThread]
         static void Main()
         {
-
-            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-
-            
-
-            jarvis = new Terminal();
-            Application.Run(jarvis);
-            
-        }
-        public static void something()
-        {
-            using (SpeechRecognitionEngine recognizer =
-             new SpeechRecognitionEngine(new System.Globalization.CultureInfo("en-US")))
-            {
-
-                //// Create a grammar.
-                ////  Create lists of alternative choices.
-                //Choices listTypes = new Choices(new string[] { "albums", "artists" });
-                //Choices genres = new Choices(new string[] { "blues", "classical", "gospel", "jazz", "rock" });
-
-                ////  Create a GrammarBuilder object and assemble the grammar components.
-                //GrammarBuilder mediaMenu = new GrammarBuilder("Display the list of");
-                //mediaMenu.Append(listTypes);
-                //mediaMenu.Append("in the");
-                //mediaMenu.Append(genres);
-                //mediaMenu.Append("category.");
-
-                //  Build a Grammar object from the GrammarBuilder.
-                //Grammar mediaMenuGrammar = new Grammar(mediaMenu);
-                //mediaMenuGrammar.Name = "Media Chooser";
-
-                // Attach event handlers.
-            
-                recognizer.SpeechRecognized +=
-                  new EventHandler<SpeechRecognizedEventArgs>(recognizer_SpeechRecognized);
-                recognizer.SpeechHypothesized +=
-                  new EventHandler<SpeechHypothesizedEventArgs>(recognizer_SpeechHypothesized);
-
-                //// Load the grammar object to the recognizer.
-                //recognizer.LoadGrammarAsync(mediaMenuGrammar);
-
-                DictationGrammar dg = new DictationGrammar();
-
-                recognizer.LoadGrammar(dg);
-
-                // Set the input to the recognizer.
-                recognizer.SetInputToDefaultAudioDevice();
-
-                // Start asynchronous recognition.
-                recognizer.RecognizeAsync();
-
-                // Keep the console window open.
-                Console.ReadLine();
-            }
-        
-
-        }
-        // Handle the SpeechHypothesized event.
-        static void recognizer_SpeechHypothesized(object sender, SpeechHypothesizedEventArgs e)
-        {
-           jarvis.setHypothesized(e.Result.Text);
+            fridayConsole = new FridayConsole();
+            Application.Run(fridayConsole);
 
         }
 
-        // Handle the SpeechRecognized event.
-        static void recognizer_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
-        {
-            jarvis.setUnderstood(e.Result.Text);
-        }
-
-     
     }
 }
 
